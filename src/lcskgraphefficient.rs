1f0c62071cc6bfd8e584369b10415ad60a38d7a6
minindu0 <mw8511@live.com>
2024-04-18 17:00:08 -0500

    small bug fix

--- src/lcskgraphefficient.rs
+++ src/lcskgraphefficient.rs
@@ -17,26 +17,23 @@ pub fn lcskpp_graph(kmer_pos_vec: Vec<(u32, u32)>, kmer_path_vec: Vec<Vec<usize>
 
     let k = k as u32;
 
-    let mut events: Vec<(u32, u32, u32, u32, u32, Vec<usize>)> = Vec::new();
+    let mut events: Vec<(u32, u32, u32, u32, Vec<usize>)> = Vec::new();
     let mut max_ns = vec![0; paths.len()];
     // generate the required events
     for (idx, &(x, y)) in kmer_pos_vec.iter().enumerate() {
         // in path find y and add k to the index and that is the value, get one path just use the first one
         let mut y_plusk_index = 0;
-        let mut y_plusk_minus1_index = 0;
-        // got y + k - 1, get the required values y - k - 1 and y - 1 convert this to binary search later
+        // this can be done in the kmer match finder
         if let Some(index_path) = paths[kmer_path_vec[idx][0]].iter().position(|r| r == &(y as usize)) {
             y_plusk_index = paths[kmer_path_vec[idx][0]][index_path - 1 + k as usize] as u32;
-            // this has to be done for every path
-            y_plusk_minus1_index = paths[kmer_path_vec[idx][0]][index_path - 2 + k as usize] as u32;
         }
         else {
             print!("SERIOUS ERROR Y IN PATH NOT FOUND");
         }
         // add the previous one as well
         // IF END SAVE y - K - 1 NODE IF START SAVE y - 1 NODE (-1 if not available)
-        events.push((x, y, y_plusk_index, y_plusk_minus1_index, (idx + kmer_pos_vec.len()) as u32, kmer_path_vec[idx].clone()));
-        events.push((x + k - 1, y, y_plusk_index, y_plusk_minus1_index, idx as u32, kmer_path_vec[idx].clone()));
+        events.push((x, y, y_plusk_index, (idx + kmer_pos_vec.len()) as u32, kmer_path_vec[idx].clone()));
+        events.push((x + k - 1, y, y_plusk_index, idx as u32, kmer_path_vec[idx].clone()));
         for path in kmer_path_vec[idx].clone() {
             max_ns[path] = max(max_ns[path], x + k - 1);
             max_ns[path] = max(max_ns[path], y_plusk_index);
@@ -44,9 +41,6 @@ pub fn lcskpp_graph(kmer_pos_vec: Vec<(u32, u32)>, kmer_path_vec: Vec<Vec<usize>
     }
     // sorting is okay with topologically converted indices
     events.sort_unstable();
-    for event in &events {
-        println!("{} {} {} {} {:?}", event.0, event.1, event.2, event.3, event.4);
-    }
     let mut max_bit_tree_path = vec![];
     // generate empty fenwick trees
     for (_index, n) in max_ns.iter().enumerate() {
@@ -59,58 +53,73 @@ pub fn lcskpp_graph(kmer_pos_vec: Vec<(u32, u32)>, kmer_path_vec: Vec<Vec<usize>
     dp.resize(events.len(), (0, 0));
     for ev in events {
         // p is the match index
-        let p = (ev.4 % kmer_pos_vec.len() as u32) as usize;
-        println!("x:{} y_start:{} y_end:{} k-1:{}  p:{} idx:{} paths:{:?}", ev.0, ev.1, ev.2, ev.3, p, ev.4, ev.5);
+        let p = (ev.3 % kmer_pos_vec.len() as u32) as usize;
+        println!("x:{} y_start:{} y_end:{} p:{} idx:{} paths:{:?}", ev.0, ev.1, ev.2, p, ev.3, ev.4);
         // is start if higher than this
-        let is_start = ev.4 >= (kmer_pos_vec.len() as u32);
+        let is_start = ev.3 >= (kmer_pos_vec.len() as u32);
         if is_start {
             print!("IS START \n");
             dp[p] = (k, -1);
             // go through the paths available in this event, and get the max corrosponding value and pos
-            for path in ev.5 {
+            for path in &ev.4 {
                 // NEED TO MODIFY THIS TO TAKE THE PREVIOUS NODE IN PATH // do binary search to save time later
-                if let Some(index_path) = paths[path].iter().position(|r| r == &(ev.1 as usize)) {
-                    let j = paths[path][index_path - 1] as u32;
-                    println!("j value = {}", j);
-                    let (temp_value, temp_position) = max_bit_tree_path[path].get(j as usize);
-                    println!("temp value from fenwick tree {}", temp_value);
-                    if (temp_value + k > dp[p].0) && (temp_value > 0) {
-                        dp[p] = (k + temp_value, temp_position as i32);
-                        best_dp = max(best_dp, (dp[p].0, p as i32, path));
+                if let Some(index_path) = paths[*path].iter().position(|r| r == &(ev.1 as usize)) {
+                    if index_path > 0 {
+                        let j = paths[*path][index_path - 1] as u32;
+                        println!("j value = {}", j);
+                        let (temp_value, temp_position) = max_bit_tree_path[*path].get(j as usize);
+                        println!("temp value from fenwick tree {}", temp_value);
+                        if (temp_value + k > dp[p].0) && (temp_value > 0) {
+                            dp[p] = (k + temp_value, temp_position as i32);
+                            best_dp = max(best_dp, (dp[p].0, p as i32, *path));
+                            println!("best_dp {}", best_dp.0);
+                        }
                     }
                 }
-                
-            } // done until here
+            }
         } else {
             print!("IS END \n");
             // See if this kmer continues a different kmer
-            if ev.0 > k && ev.1 > k {
+            if ev.0 >= k {
                 // check the diagonally prev slot for a kmer
-                // need the events path and find the k - 1 from path to find p start of continueing so event need the path index as well
-                if let Ok(cont_idx) = kmer_pos_vec.binary_search(&(ev.0 - k - 1, ev.1 - 1)) {
-                    let prev_score = dp[cont_idx].0;
-                    let candidate = (prev_score + 1, cont_idx as i32);
-                    dp[p] = max(dp[p], candidate);
-                    best_dp = max(best_dp, (dp[p].0, p as i32, 0));
-                    println!("cont value from candidate {}", candidate.0);
+                // we can just prev node in graph have to modify this *********
+                println!("candidate location {} {}", ev.0 - k, ev.1 - 1);
+                for path in &ev.4 {
+                    if let Some(index_path) = paths[*path].iter().position(|r| r == &(ev.1 as usize)) {
+                        if index_path > 0 {
+                            let ev1_minus_1 = paths[*path][index_path - 1] as u32;
+                            if let Ok(cont_idx) = kmer_pos_vec.binary_search(&(ev.0 - k, ev1_minus_1)) {
+                                let prev_score = dp[cont_idx].0;
+                                let candidate = (prev_score + 1, cont_idx as i32);
+                                dp[p] = max(dp[p], candidate);
+                                best_dp = max(best_dp, (dp[p].0, p as i32, *path));
+                                println!("candidate location {} {}", ev.0 - k, ev1_minus_1);
+                                println!("cont value from candidate p {} score {}", cont_idx, candidate.0);
+                                println!("best_dp {}", best_dp.0);
+                            }
+                        }
+                    }
                 }
             }
             // set all trees which have this match as this 
-            for path in ev.5 {
-                max_bit_tree_path[path].set(ev.2 as usize, (dp[p].0, p as u32));
+            for path in &ev.4 {
+                max_bit_tree_path[*path].set(ev.2 as usize, (dp[p].0, p as u32));
             }
         }
     }
-    //let mut traceback = Vec::new();
-    //let (best_score, mut prev_match) = best_dp;
-    //while prev_match >= 0 {
-    //    traceback.push(prev_match as usize);
-    //    prev_match = dp[prev_match as usize].1;
-    //}
+    let mut traceback = Vec::new();
+    let (best_score, mut prev_match, mut path) = best_dp;
+    println!("BEST SCORE: {} PREV_MATCH: {}", best_score, prev_match);
+    println!("PATHS");
+    while prev_match >= 0 {
+        println!("{} ", prev_match);
+        traceback.push(prev_match as usize);
+        prev_match = dp[prev_match as usize].1;
+    }
     //traceback.reverse();
     //path :traceback;
     //score: best_score,
-    //dp_vector: dp, 
+    //dp_vector: dp,
 }
 
 pub fn convert_topological_indices_to_ascending_indices (topo_indices: &Vec<usize>, paths: &Vec<Vec<usize>>, matches: &Vec<(u32, u32)>) -> (Vec<Vec<usize>>, Vec<(u32, u32)>) {
