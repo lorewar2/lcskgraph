5164c694ec446dd83af18c60cd43674546ebd912
lorewar2 <mw8511@live.com>
2024-06-19 16:35:43 -0500

    only positive section scores

--- src/main.rs
+++ src/main.rs
@@ -133,7 +133,10 @@ fn lcsk_test_pipeline(reads: Vec<String>, kmer_size: usize, band_size: usize) ->
             println!("graph start end {:?} length {}", graph_start_end, graph_length);
             println!("lcsk path {:?}", section_lcsk_path);
             let section_score = aligner.custom_banded_threaded(&section_query, &lcsk_path, band_size, &topo_map, section_graphs[anchor_index].clone()).alignment().score;
-            total_section_score += section_score;
+            if section_score > 0 {
+                total_section_score += section_score;
+            }
+            
             println!("section score {}", section_score);
         }
         println!("total section score {}", total_section_score);
